0467fb098e7b6a06dc56d386d3ce37e9c3164843
pgd <rudeh1253@gmail.com>
2024-08-09 17:07:41 +0900

    Programmers(Lv0): 창고정리 - https://school.programmers.co.kr/learn/courses/30/lessons/250126

---
+++ programmers/java/Lv0_Storage.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/250126
+public class Lv0_Storage {
+
+    public String solution(String[] storage, int[] num) {
+        List<String> cleanItems = new ArrayList<>();
+        List<Integer> numOfItems = new ArrayList<>();
+
+        String currentItem = null;
+        int accumulator = 0;
+        for (int i = 0; i < storage.length; i++) {
+            String item = storage[i];
+            int itemNum = num[i];
+            if (item.equals(currentItem)) {
+                accumulator += itemNum;
+            } else {
+                if (currentItem != null) {
+                    cleanItems.add(currentItem);
+                    numOfItems.add(accumulator);
+                }
+                currentItem = item;
+                accumulator = itemNum;
+            }
+        }
+
+        int maxIdx = -1;
+        int max = 0;
+        for (int i = 0; i < numOfItems.size(); i++) {
+            if (numOfItems.get(i) > max) {
+                max = numOfItems.get(i);
+                maxIdx = i;
+            }
+        }
+        return cleanItems.get(maxIdx);
+    }
+}
